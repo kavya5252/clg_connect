9f4ca1026345bc8d709bbf0a495488019ba92321
kavya5252 <133213039+kavya5252@users.noreply.github.com>
2024-12-31 14:12:26 +0530

    Create comments.inc.php

---
+++ comments.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+function setComments($conn){
+     if(isset($_POST['commentSubmit'])){
+        $uid= $_POST['uid'];
+        $date= $_POST['date'];
+        $message= $_POST['message'];
+
+        $sql = " INSERT INTO comments (uid,date,message)
+         VALUES ('$uid', '$date', '$message')";
+
+         $result = $conn->query($sql);
+     }
+}
+
+function getcomments($conn){
+      $sql = "SELECT * FROM comments";
+      $result = $conn->query($sql);
+      while( $row =$result->fetch_assoc()){
+        echo"<div class='comment-box'><p>";
+        echo  $row['uid']."<br>";
+        echo  $row['date']."<br>";
+        echo nl2br($row['message']);
+        echo "</p></div>";
+
+      }
+      $row =$result->fetch_assoc();
+      
+
+}
